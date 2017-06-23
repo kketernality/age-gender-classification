301adff42de7b2b4cf25e3a5030c9a41e97349ea
Chang-Yi <kketernality@gmail.com>
2017-06-23 19:34:09 +0800

    add download_models script

---
+++ dowload_models.sh
@@ -0,0 +1,3 @@
+cd models
+wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/dex_imdb_wiki.caffemodel -O age.caffemodel
+wget https://data.vision.ee.ethz.ch/cvl/rrothe/imdb-wiki/static/gender.caffemodel -O gender.caffemodel
